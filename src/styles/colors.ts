b0ec6c4d6742ea1d5311092579dbedfcac097256
TaeminMoon <tmmoond8@gmail.com>
2020-09-26 18:41:51 +0900

    Update: color set

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,7 +3,11 @@ export default {
   black: '#000000',
   black50: '#080d1f',
   black100: '#1f1f1f',
-  blackBF: '#bfbfbf',
+  black66: '#666666',
   black99: '#999999',
+  blackBF: '#bfbfbf',
+  blackD9: '#d9d9d9',
+  blackEB: '#ebebeb',
+  blackF5F6F7: '#f5f6f7',
   primary: '#2346cf',
 };
