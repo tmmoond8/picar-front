72e4f8ef2a94216e12270963cf75774252beca87
TaeminMoon <tmmoond8@gmail.com>
2020-09-29 20:23:49 +0900

    feature: Acticle Footer 작성

--- src/components/Article/ArticleFooter.tsx
+++ src/components/Article/ArticleFooter.tsx
@@ -26,7 +26,19 @@ export default function ArticleFooter(props: ArticleFooterProps): JSX.Element {
         <Icon icon="emojiLove" size="18px" />
         <span>{emojiHappy}</span>
       </InteractionCounter>
-      <InteractionPanel />
+      <InteractionPanel>
+        <ul>
+          <li>
+            <Icon icon="emojiSmile" size="20px" /> 공감표현
+          </li>
+          <li>
+            <Icon icon="chatOutline" size="20px" /> 댓글쓰기
+          </li>
+          <li>
+            <Icon icon="emojiSmile" size="20px" /> 공감표현
+          </li>
+        </ul>
+      </InteractionPanel>
     </Self>
   );
 }
@@ -59,4 +71,26 @@ const InteractionCounter = styled.div`
     margin-left: 5px;
   }
 `;
-const InteractionPanel = styled.div``;
+const InteractionPanel = styled.div`
+  height: 54px;
+  color: ${colors.black66};
+  box-shadow: inset 0 0.5px 0 0 ${colors.blackEB};
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 307px;
+    height: 100%;
+    margin: 0 auto;
+    font-size: 14px;
+    li {
+      display: flex;
+      align-items: center;
+
+      svg {
+        margin-right: 6px;
+      }
+    }
+  }
+`;
