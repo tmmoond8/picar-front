7ac76819ec9ff5cf249c36546d904ac0f43ef8c7
TaeminMoon <tmmoond8@gmail.com>
2020-09-29 20:07:57 +0900

    feat: icon 추가

--- src/components/Icon/icons/index.ts
+++ src/components/Icon/icons/index.ts
@@ -3,3 +3,6 @@ export { ReactComponent as dropdown } from './dropdown.svg';
 export { ReactComponent as like } from './like.svg';
 export { ReactComponent as chat } from './chat.svg';
 export { ReactComponent as emojiLove } from './emoji-love.svg';
+export { ReactComponent as emojiSmile } from './emoji-smile.svg';
+export { ReactComponent as chatOutline } from './chat-outline.svg';
+export { ReactComponent as externalLink } from './external-link.svg';
