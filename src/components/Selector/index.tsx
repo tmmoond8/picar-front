9fe0695c084083da22a184e3a049a07293634940
TaeminMoon <tmmoond8@gmail.com>
2020-10-01 14:12:19 +0900

    feat: Selector 컴포넌트 작성

---
+++ src/components/Selector/index.tsx
@@ -0,0 +1,48 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+import React, { ChangeEvent } from 'react';
+
+import { colors } from '../../styles';
+
+interface SelectorProps {
+  items: string[];
+  selected: string;
+  handleChange: (value: string) => void;
+}
+
+export default function Selector(props: SelectorProps): JSX.Element {
+  const { items, handleChange, selected } = props;
+
+  const handleChangeSelect = React.useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      handleChange(e.target.value as string);
+    },
+    [handleChange],
+  );
+  return (
+    <StyledSelector onChange={handleChangeSelect}>
+      {items.map((item) => (
+        <option value={item} selected={selected === item}>
+          {item}
+        </option>
+      ))}
+    </StyledSelector>
+  );
+}
+
+const StyledSelector = styled.select`
+  height: 24px;
+  padding: 0 15px 0 0;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  color: ${colors.black22};
+  appearance: none;
+`;
+
+export const useSelector = (items: string[], defaultValue?: string) => {
+  return React.useState<string>(() =>
+    items.length > 0 ? defaultValue || items[0] : '',
+  );
+};
