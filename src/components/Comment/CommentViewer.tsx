bcc324ce1212de7e39a1217bf90aa8de79dbc50d
TaeminMoon <tmmoond8@gmail.com>
2020-12-05 01:59:55 +0900

    feat: 댓글 없을 때 처리

---
+++ src/components/Comment/CommentViewer.tsx
@@ -0,0 +1,97 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { colors } from '../../styles';
+import Icon from '../Icon';
+import Comment from './Comment';
+import CommentType from '../../types/Comment';
+import CommentEditor from './CommentEditor';
+
+const CommentViewer: React.FC<{ comments: CommentType[] }> = ({ comments }) => {
+  return (
+    <StyledComments>
+      <CommentList>
+        {comments.map((comment) => (
+          <Comment
+            key={comment.id}
+            id={comment.id}
+            authorCode={comment.author.code}
+            name={comment.author.name}
+            group={comment.author.group}
+            createAt={comment.createAt}
+            content={comment.content}
+            thumbnail={comment.author.thumbnail}
+            isDelete={comment.isDelete}
+          >
+            <ReplyList>
+              {comment.replies &&
+                comment.replies.map((reply) => (
+                  <Comment
+                    key={reply.id}
+                    id={reply.id}
+                    authorCode={reply.author.code}
+                    name={reply.author.name}
+                    group={reply.author.group}
+                    createAt={reply.createAt}
+                    content={reply.content}
+                    thumbnail={reply.author.thumbnail}
+                    isDelete={reply.isDelete}
+                  />
+                ))}
+            </ReplyList>
+          </Comment>
+        ))}
+        {comments.length === 0 && (
+          <Empty>
+            <Icon icon="chat" size="39px" />
+            <h3>첫 댓글을 남겨주세요.</h3>
+          </Empty>
+        )}
+      </CommentList>
+      <CommentEditor />
+    </StyledComments>
+  );
+};
+
+export default React.memo(CommentViewer);
+
+const StyledComments = styled.div`
+  padding: 0 0 92px;
+`;
+
+const CommentList = styled.ol`
+  min-height: 240px;
+  overflow: hidden;
+  label: CommentList;
+`;
+
+const ReplyList = styled.ol`
+  padding: 0 0 0 44px;
+  & > li {
+    background-color: white;
+    box-shadow: -100px 0 white, 150px 0 white, 250px 0 white, 350px 0 white,
+      450px 0 white, 550px 0 white, 650px 0 white, 750px 0 white, 850px 0 white,
+      950px 0 white, 1050px 0 white, 950px 0 white, 1250px 0 white,
+      1150px 0 white, 1450px 0 white, 1350px 0 white, 1650px 0 white,
+      1550px 0 white;
+  }
+  label: ReplyList;
+`;
+
+const Empty = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 240px;
+
+  h3 {
+    margin-top: 13px;
+    font-size: 15px;
+    font-weight: normal;
+    line-height: normal;
+    color: ${colors.blackBF};
+  }
+`;
--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,12 +1,9 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import styled from '@emotion/styled';
 import React from 'react';
 
-import Comment from './Comment';
-import CommentEditor from './CommentEditor';
-
 import CommentContext from './context';
+import CommentViewer from './CommentViewer';
 import { useFetch, useWriteComment, useAbout, useRemoveComment } from './hooks';
 
 const CommentArea: React.FC<{
@@ -50,73 +47,9 @@ const CommentArea: React.FC<{
         about,
       }}
     >
-      <Area>
-        <CommentList>
-          {comments.map((comment) => (
-            <Comment
-              key={comment.id}
-              id={comment.id}
-              authorCode={comment.author.code}
-              name={comment.author.name}
-              group={comment.author.group}
-              createAt={comment.createAt}
-              content={comment.content}
-              thumbnail={comment.author.thumbnail}
-              isDelete={comment.isDelete}
-            >
-              <ReplyList>
-                {comment.replies &&
-                  comment.replies.map((reply) => (
-                    <Comment
-                      key={reply.id}
-                      id={reply.id}
-                      authorCode={reply.author.code}
-                      name={reply.author.name}
-                      group={reply.author.group}
-                      createAt={reply.createAt}
-                      content={reply.content}
-                      thumbnail={reply.author.thumbnail}
-                      isDelete={reply.isDelete}
-                    />
-                  ))}
-              </ReplyList>
-            </Comment>
-          ))}
-        </CommentList>
-        <CommentEditor />
-      </Area>
+      <CommentViewer comments={comments} />
     </CommentContext.Provider>
   );
 };
 
 export default CommentArea;
-
-const Area = styled.div`
-  padding: 0 0 92px;
-  h3 {
-    height: 59px;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 59px;
-    letter-spacing: -0.25px;
-    color: #3e4045;
-  }
-`;
-
-const CommentList = styled.ol`
-  overflow: hidden;
-  label: CommentList;
-`;
-
-const ReplyList = styled.ol`
-  padding: 0 0 0 44px;
-  & > li {
-    background-color: white;
-    box-shadow: -100px 0 white, 150px 0 white, 250px 0 white, 350px 0 white,
-      450px 0 white, 550px 0 white, 650px 0 white, 750px 0 white, 850px 0 white,
-      950px 0 white, 1050px 0 white, 950px 0 white, 1250px 0 white,
-      1150px 0 white, 1450px 0 white, 1350px 0 white, 1650px 0 white,
-      1550px 0 white;
-  }
-  label: ReplyList;
-`;
