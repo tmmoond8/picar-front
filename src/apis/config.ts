ae6f4b02832cd2eb39974f5c64e11a035924bcab
TaeminMoon <tmmoond8@gmail.com>
2020-12-22 00:43:54 +0900

    Update: 개발환경에서 api 주소 변경

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -2,8 +2,11 @@ import axios, { AxiosInstance } from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const apiUrl = process.env.NODE_ENV ===  'development'
+  ? `${window.location.origin}/api`.replace(':3000', ':4040')
+  : process.env.REACT_APP_API_URL 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: apiUrl,
   withCredentials: true,
 });
 
