3e99419ab3c8914409de4e06b0717c49b7948b06
tmmoond8 <tmmoond8@gmail.com>
2021-02-11 15:16:42 +0900

    Revert "feat: 데탑일 때 애니메이션 없이 팝업 팍 뜨기"
    This reverts commit e86c76b17227267871864a53393ea8020a8a4d9d.

--- src/components/Modal/hooks.tsx
+++ src/components/Modal/hooks.tsx
@@ -1,27 +1,20 @@
 /* eslint-disable no-underscore-dangle */
 import { ModalData } from './ModalViewer';
 import global from '../../types/global';
-import { useStore } from '../../stores';
 
 let isOpen = false;
 
 export const useModal = () => {
-  const { ui }  = useStore();
   const id = `modal${Math.random().toString(32).split('.')[1]}`;
   const close = () => {
     isOpen = false;
     const modalEl: HTMLElement | null = document.querySelector(`#${id}`);
-    if (modalEl && ui.queryMatch.Mobile) {
+    if (modalEl) {
       modalEl.style!.transform = 'translateY(100vh)';
     }
-
-    if (ui.queryMatch.Mobile) {
-      setTimeout(() => {
-        global.__OWNER__.closeModal(id);
-      }, 300);
-    } else {
+    setTimeout(() => {
       global.__OWNER__.closeModal(id);
-    }
+    }, 300);
   };
 
   const open = (modal: Omit<ModalData, 'id' | 'handleClose'>) => {
