dc877ad17932674fc70ed38530b37c2547fa9151
tamm <tmmoond8@gmail.com>
2021-02-28 20:20:09 +0900

    fix: Squircle 이 각지게 바뀌는 버그

--- src/components/Squircle/index.tsx
+++ src/components/Squircle/index.tsx
@@ -15,20 +15,21 @@ const Squircle: React.FC<{
       onClick(e);
     }
   }, [onClick]);
+  const { shapeSquircle, clipSquircle } = useUUID();
   return (
     <Wrapper width={size} className={cx('UserProfilePhoto', className)} onClick={handleClick}>
       <svg viewBox="0 0 88 88">
         <defs>
-          <path id="shapeSquircle" d="M44,0 C76.0948147,0 88,11.9051853 88,44 C88,76.0948147 76.0948147,88 44,88 C11.9051853,88 0,76.0948147 0,44 C0,11.9051853 11.9051853,0 44,0 Z"></path>
-          <clipPath id="clipSquircle">
-            <use xlinkHref="#shapeSquircle"/>
+          <path id={shapeSquircle} d="M44,0 C76.0948147,0 88,11.9051853 88,44 C88,76.0948147 76.0948147,88 44,88 C11.9051853,88 0,76.0948147 0,44 C0,11.9051853 11.9051853,0 44,0 Z"></path>
+          <clipPath id={clipSquircle}>
+            <use xlinkHref={`#${shapeSquircle}`}/>
           </clipPath>
         </defs>
 
         <image width="100%" 
           height="100%"  
           preserveAspectRatio="xMidYMid slice"
-          clip-path="url(#clipSquircle)" 
+          clip-path={`url(#${clipSquircle})`} 
           xlinkHref={src}
         />
       </svg>
@@ -44,3 +45,11 @@ const Wrapper = styled.div<{ width: number}>`
     height: auto;
   }
 `;
+
+function useUUID() {
+  const uuid = React.useMemo(() => Math.random().toString(32).substr(4), []);
+  return {
+    shapeSquircle: `shapeSquircle_${uuid}`,
+    clipSquircle: `clipSquircle_${uuid}` 
+  }
+}
