326b933fc1d7d37c90554c970fdea40d27288318
tamm <tmmoond8@gmail.com>
2021-03-11 22:10:05 +0900

    feat: open-graph 업데이트

--- src/components/OwwnersHelmet/index.tsx
+++ src/components/OwwnersHelmet/index.tsx
@@ -23,15 +23,18 @@ const OwwnersHelmet = (props: Partial<Header>) => {
       <meta charSet="utf-8" />
       <title>{props.title ?? headerData.title}</title>
       <meta name="description" content={props.description ?? headerData.description} />
-      <meta name="og:title" content={props.title ?? headerData.title} />
-      <meta name="og:description" content={props.description ?? headerData.description} />
-      <meta name="og:type" content="website" />
-      <meta name="og:image" content={props.image ?? headerData.image} />
-      <meta name="og:url" content={props.url ?? headerData.url}/>
-      <meta name="twitter:card" content="summary"/>
-      <meta name="twitter:creator" content="Tamm"/>
-      <meta name="twitter:title" content={props.title ?? headerData.title}/>
-      <meta name="twitter:description" content={props.description ?? headerData.description}/>
+      <meta property="og:title" content={props.title ?? headerData.title} />
+      <meta property="og:description" content={props.description ?? headerData.description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={props.image ?? headerData.image} />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="200" />
+      <meta property="og:url" content={props.url ?? headerData.url}/>
+      <meta property="twitter:card" content="summary"/>
+      <meta property="twitter:creator" content="Tamm"/>
+      <meta property="twitter:title" content={props.title ?? headerData.title}/>
+      <meta property="twitter:description" content={props.description ?? headerData.description}/>
       <link rel="apple-touch-icon" href={props.favicon ?? headerData.favicon} />
       <link rel="apple-touch-icon" sizes="64x64" href={props.favicon ?? headerData.favicon} /> 
       <link rel="canonical" href={props.url ?? headerData.url} />
