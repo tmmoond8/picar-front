36d6b6cd3746dd513c36ab79339e086a5b0ee8fa
tamm <tmmoond8@gmail.com>
2021-04-01 21:12:22 +0900

    fix: 스크롤바 아얘 감추기

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -8,6 +8,10 @@ const customReset = css`
     font-family: 'Noto Sans KR', sans-serif;
   }
 
+  *::-webkit-scrollbar {
+    display: none;
+  }
+
   body {
     background-color: ${colors.blackF5F6F7};
   }
