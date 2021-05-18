9bd57ac2eb7fe8d080cdc21a06376ddfb81d97e5
tamm <tmmoond8@gmail.com>
2021-05-18 23:35:34 +0900

    feat: react-snap 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import App from './App';
 import { MobxProvider } from './stores';
 import * as serviceWorker from './serviceWorker';
 import UiProvider from './components/UiProvider';
 
-ReactDOM.render(
+const OwwnersApp = () => (
   <React.StrictMode>
     <MobxProvider>
       <UiProvider>
         <App />
       </UiProvider>
     </MobxProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+  </React.StrictMode>
+)
+
+const rootElement = document.getElementById("root");
+if (rootElement!.hasChildNodes()) {
+  hydrate(<OwwnersApp />, rootElement);
+} else {
+  render(<OwwnersApp />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
