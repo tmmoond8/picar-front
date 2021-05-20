e6f7a9872f1b0a2a46e5bb2c452f637dd3b8ba40
tamm <tmmoond8@gmail.com>
2021-05-20 21:24:39 +0900

    feat: 페이지 업데이트

---
+++ public/static/js/54.f0ad1c20.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpowner-front"]=this["webpackJsonpowner-front"]||[]).push([[54],{363:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return r}));var e=o(44),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,o=n.innerHeight,r=document.elementFromPoint(t/2,o/2);if(r){var i=r.closest("ion-content");i&&i.componentOnReady().then((function(){Object(e.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.f0ad1c20.chunk.js.map
