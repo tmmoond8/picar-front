2ed329c90881055069d0704f941df36b51b087ef
tamm <tmmoond8@gmail.com>
2021-06-05 00:57:09 +0900

    feat: react-app-rewired 설치해서 babel 커스텀

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const {
+  addDecoratorsLegacy,
+  disableEsLint,
+  override,
+  useBabelRc,
+} = require('customize-cra');
+
+module.exports = {
+  webpack: override(disableEsLint(), addDecoratorsLegacy(), useBabelRc()),
+};
