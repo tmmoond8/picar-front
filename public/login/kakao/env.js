8a2eda8647cd8986b82b1bc802fa5d150907c420
tamm <tmmoond8@gmail.com>
2021-06-13 21:30:16 +0900

    feat: 로그인 되어라

--- public/login/kakao/env.js
+++ public/login/kakao/env.js
@@ -1,7 +1,7 @@
 const develop = {
   KAKAO_LOGIN_KEY: '29bd68786fc799a70d7a8a0ec18b93ee',
   KAKAO_USER_API_KEY: '2894e96c1db14062fc4b2f2cf4119641',
-  KAKAO_LOGIN_BRIDGE_URL: 'http://192.168.1.165:8200/login/kakao/index.html',
+  KAKAO_LOGIN_BRIDGE_URL: 'http://192.168.1.165:8200/login/kakao/',
   API_URL: 'http://192.168.1.165:6060/api',
 };
 
--- public/login/naver/env.js
+++ public/login/naver/env.js
@@ -1,14 +1,14 @@
 const develop = {
   NAVER_CLIENT_ID: 'd3dL6VA0QQaGHiVMswZ2',
   NAVER_CLIENT_SECRET: 'eXGhuyO9QK',
-  NAVER_LOGIN_BRIDGE_URL: 'http://192.168.1.165:8200/login/naver/index.html',
+  NAVER_LOGIN_BRIDGE_URL: 'http://192.168.1.165:8200/login/naver/',
   API_URL: 'http://192.168.1.165:6060/api',
 };
 
 const production = {
   NAVER_CLIENT_ID: 'vFqCamXUUg2N8fsuScEE',
   NAVER_CLIENT_SECRET: 'nC9nDXXnLk',
-  NAVER_LOGIN_BRIDGE_URL: 'https://www.picar.kr/login/naver/index.html',
+  NAVER_LOGIN_BRIDGE_URL: 'https://www.picar.kr/login/naver/',
   API_URL: 'https://api.picar.kr/api',
 };
 
