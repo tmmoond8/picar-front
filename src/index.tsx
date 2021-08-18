db2f3efc94ae5786f98ac52ee263a5611802de7b
tmmoond8 <tmmoond8@gmail.com>
2021-08-18 12:41:00 +0900

    🎨 App 컴포넌트 구조 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import App from './App';
-import { MobxProvider } from './stores';
 import * as serviceWorker from './serviceWorker';
-import UiProvider from './components/UiProvider';
-
-const Root: React.FC = () => (
-  <React.StrictMode>
-    <MobxProvider>
-      <UiProvider>
-        <App />
-      </UiProvider>
-    </MobxProvider>
-  </React.StrictMode>
-)
 
 const rootElement = document.getElementById("root");
 if (rootElement!.hasChildNodes()) {
-  hydrate(<Root />, rootElement);
+  hydrate(<App />, rootElement);
 } else {
-  render(<Root />, rootElement);
+  render(<App />, rootElement);
 }
 
 // If you want your app to work offline and load faster, you can change
