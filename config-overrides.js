e1016b8b619f1aa2462298eb5513722559ade981
tmmoond8 <tmmoond8@gmail.com>
2021-08-20 23:24:42 +0900

    🚀 serverless 파일 정의

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {
   addDecoratorsLegacy,
   disableEsLint,
@@ -5,6 +6,34 @@ const {
   useBabelRc,
 } = require('customize-cra');
 
+const hash = process.env.COMMIT_REF ? `.${process.env.COMMIT_REF}` : '';
+
+const output = () => (config) => {
+  // 기본 js 파일명 변경
+  config.output.filename = `static/js/[name]${hash}.js`
+  config.output.chunkFilename = `static/js/[name]${hash}.chunk.js`;
+  config.plugins.forEach(plugin => {
+    if (plugin && plugin.options) {
+      const { filename } = plugin.options;
+      // html 파일 명 번경
+      if (filename === 'index.html') {
+        // plugin.options.filename = 'server.html';
+      }
+      // 기본 css 파일명 변경
+      if (filename && filename.includes('static/css')) {
+        plugin.options.filename = `static/css/[name]${hash}.css`;
+        plugin.options.chunkFilename = `static/css/[name]${hash}.chunk.css`;
+      }
+    }
+  })
+  return config;
+}
+
 module.exports = {
-  webpack: override(disableEsLint(), addDecoratorsLegacy(), useBabelRc()),
+  webpack: override(
+    disableEsLint(), 
+    addDecoratorsLegacy(), 
+    useBabelRc(),
+    output(),
+  ),
 };
