c3b04728e2164be84a40ac146fa984bf0b25556f
tmmoond8 <tmmoond8@gmail.com>
2021-09-01 01:15:48 +0900

    🚨  sitemap 최신 리스트 가져올 때 sync-fetch 사용하기

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -1,34 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const today = new Date().toISOString();
-const axios = require('axios');
+const fetch = require('sync-fetch');
 
-const template = `
-<?xml version="1.0" encoding="UTF-8"?>
-  <urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <sitemap>
+const template = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
       <loc>https://www.picar.kr</loc>
       <lastmod>${today}</lastmod>
-    </sitemap>
-    {{sitemap}}
+    </url>{{sitemap}}
   </urlset>`
 
-const API_URL = 'https://www.picar.kr';
+const API_URL = 'https://api.picar.kr';
 // const API_URL = 'http://localhost:6060';
 
-axios.get(`${API_URL}/api/article/last`)
-  .then(({ data: { data } }) => {
-    const sitemap = Array.from({ length: data.id}).map((_, idx) => `
-      <sitemap>
-        <loc>https://www.picar.kr/article/${idx + 1}</loc>
-        <lastmod>${today}</lastmod>
-      </sitemap>`).join('')
+try {
+  const { data } = fetch(`${API_URL}/api/article/last`).json();
+  const sitemap = Array.from({ length: data.id}).map((_, idx) => `
+    <url>
+      <loc>https://www.picar.kr/article/${idx + 1}</loc>
+      <lastmod>${today}</lastmod>
+    </url>`).join('')
 
-    fs.writeFileSync(path.resolve(__dirname, '../build/sitemap.xml'), template.replace('{{sitemap}}', sitemap), 'utf8')
-  })
-
-
+  fs.writeFileSync(path.resolve(__dirname, '../build/sitemap.xml'), template.replace('{{sitemap}}', sitemap), 'utf8')
+} catch(error ) {
+  console.error(error);
+}
 
