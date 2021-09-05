7d87f7619ed49e111769bd237949d266b8f946c5
tmmoond8 <tmmoond8@gmail.com>
2021-09-06 00:57:34 +0900

    🔥 불필요한 모듈 및 코드 제거

--- config-overrides.js
+++ config-overrides.js
@@ -5,28 +5,10 @@ const {
   useBabelRc,
 } = require('customize-cra');
 
-const output = () => (config) => {
-  // 기본 js 파일명 변경
-  config.output.filename = `static/js/[name]${hash}.js`
-  config.output.chunkFilename = `static/js/[name]${hash}.chunk.js`;
-  config.plugins.forEach(plugin => {
-    if (plugin && plugin.options) {
-      const { filename } = plugin.options;
-      // 기본 css 파일명 변경
-      if (filename && filename.includes('static/css')) {
-        plugin.options.filename = `static/css/[name]${hash}.css`;
-        plugin.options.chunkFilename = `static/css/[name]${hash}.chunk.css`;
-      }
-    }
-  })
-  return config;
-}
-
 module.exports = {
   webpack: override(
     disableEsLint(), 
     addDecoratorsLegacy(), 
     useBabelRc(),
-    // output(),
   ),
 };
