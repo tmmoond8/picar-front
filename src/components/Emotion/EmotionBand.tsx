ca785c65d75b11b6ebac41b6265dc30006a78805
tmmoond8 <tmmoond8@gmail.com>
2021-10-18 21:33:44 +0900

    🎨 롤링 이모션 애니메이션 개선

--- src/components/Emotion/EmotionBand.tsx
+++ src/components/Emotion/EmotionBand.tsx
@@ -3,7 +3,6 @@
 import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
 import React from 'react';
-
 import { colors } from '../../styles';
 import { EmotionCount } from '../../types/Emotion';
 import Icon, { IconKey } from '../Icon';
@@ -14,26 +13,8 @@ const EmotionBand: React.FC<{ emotionCounts: EmotionCount[] }> = ({
   emotionCounts,
 }) => {
   const [isFold, setIsFold] = React.useState(true);
-  const ref = React.useRef<HTMLDivElement>(null);
   const [rollingHeight, setRollingHeight] = React.useState(HEIGHT_MAP[0]);
-  const handleToggleFold = React.useCallback(() => {
-    const EmotionBandElement = ref.current!;
-    if (isFold) {
-      EmotionBandElement.style.width = '100%';
-      setIsFold(!isFold);
-    } else {
-      EmotionBandElement.style.width = '170px';
-      setTimeout(() => {
-        EmotionBandElement.style.width = '108px';
-        setIsFold(!isFold);
-      }, 200);
-    }
-  }, [isFold, ref]);
-  const toggleWrapper = React.useCallback(() => {
-    if (isFold) {
-      handleToggleFold();
-    }
-  }, [handleToggleFold, isFold]);
+
   React.useEffect(() => {
     let count = 0;
     const timer = setInterval(() => {
@@ -41,84 +22,104 @@ const EmotionBand: React.FC<{ emotionCounts: EmotionCount[] }> = ({
       setRollingHeight(HEIGHT_MAP[count % 4]);
     }, 3000);
     return () => clearTimeout(timer);
-  }, [isFold]);
+  }, []);
+
   return (
-    <Band
-      ref={ref}
-      isFold={isFold}
-      rollingHeight={rollingHeight}
-      onClick={toggleWrapper}
-    >
-      <FoldingButton
-        icon={isFold ? 'leftTriangle' : 'close'}
-        size="16px"
-        color={colors.blackBF}
-        onClick={handleToggleFold}
-      />
-      <Emotions className="Emotions">
-        {emotionCounts.map((emotionCount) => (
-          <Emotion key={emotionCount.type}>
-            <Icon icon={emotionCount.icon as IconKey} size="24px" />
-            <span>{emotionCount.count}</span>
-          </Emotion>
-        ))}
-      </Emotions>
-    </Band>
+    <EmotionBandWrapper>
+      <Rolling>
+        <FoldUnFoldButton
+          icon="leftTriangle"
+          size="16px"
+          color={colors.blackBF}
+          onClick={() => setIsFold(false)}
+        />
+        <OptimizedRolingEmotions
+          rollingHeight={rollingHeight}
+          emotionCounts={emotionCounts}
+        />
+      </Rolling>
+      <Band isFold={isFold}>
+        <FoldUnFoldButton
+          icon="close"
+          size="16px"
+          color={colors.blackBF}
+          onClick={() => setIsFold(true)}
+        />
+        <BandEmotions>
+          {emotionCounts.map((emotionCount) => (
+            <Emotion key={emotionCount.type}>
+              <Icon icon={emotionCount.icon as IconKey} size="24px" />
+              <span>{emotionCount.count}</span>
+            </Emotion>
+          ))}
+        </BandEmotions>
+      </Band>
+    </EmotionBandWrapper>
   );
 };
 
 export default React.memo(EmotionBand);
 
-const Band = styled.div<{ isFold: boolean; rollingHeight: string }>`
+const OptimizedRolingEmotions = React.memo(function ({
+  rollingHeight,
+  emotionCounts,
+}: {
+  emotionCounts: EmotionCount[];
+  rollingHeight: string;
+}) {
+  return (
+    <RollingEmotions className="Emotions" rollingHeight={rollingHeight}>
+      {emotionCounts.map((emotionCount) => (
+        <Emotion key={emotionCount.type}>
+          <Icon icon={emotionCount.icon as IconKey} size="24px" />
+          <span>{emotionCount.count}</span>
+        </Emotion>
+      ))}
+    </RollingEmotions>
+  );
+});
+
+const EmotionBandWrapper = styled.div`
   position: absolute;
-  right: 0;
-  width: 100%;
   height: 100%;
-  background-color: ${colors.white};
-  transition: width 0.5s ease-out;
-  overflow: hidden;
-
-  ${(p) =>
-    p.isFold &&
-    css`
-      width: 108px;
-
-      .Emotions {
-        flex-direction: column;
-        position: absolute;
-        right: 0;
-        width: 78px;
-        height: 160px;
-        justify-content: space-between;
-        cursor: pointer;
-
-        transition: all 0.5s ease-out;
-        transform: translateY(${p.rollingHeight});
-        li {
-          margin-left: 0;
-          align-items: center;
-        }
-        svg {
-          cursor: pointer;
-        }
-      }
-    `}
+  width: 100%;
 `;
 
-const Emotions = styled.ol`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const Rolling = styled.div`
+  position: absolute;
+  right: 0;
   height: 100%;
+  width: 108px;
+  overflow: hidden;
+`;
+
+const RollingEmotions = styled.ol<{
+  rollingHeight: string;
+}>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  flex-direction: column;
+
+  right: 0;
+  width: 78px;
+  height: 160px;
+  cursor: pointer;
+
+  transition: transform 0.5s ease-in;
+  transform: translate3d(0, ${(p) => p.rollingHeight}, 0);
+
+  svg {
+    cursor: pointer;
+  }
 `;
 
 const Emotion = styled.li`
   display: flex;
   align-items: center;
+  margin-left: 20px;
 
-  & + & {
-    margin-left: 20px;
-  }
   span {
     font-size: 15px;
     color: ${colors.black66};
@@ -126,7 +127,7 @@ const Emotion = styled.li`
   }
 `;
 
-const FoldingButton = styled(Icon)`
+const FoldUnFoldButton = styled(Icon)`
   position: absolute;
   top: 50%;
   left: 18px;
@@ -134,4 +135,26 @@ const FoldingButton = styled(Icon)`
   padding: 10px;
   box-sizing: content-box;
   z-index: 10;
+`;
+
+const Band = styled.div<{ isFold: boolean }>`
+  position: absolute;
+  background-color: ${colors.white};
+  width: 100%;
+  height: 100%;
+  transition: transform 0.5s ease-in-out;
+  z-index: 20;
+
+  ${(p) =>
+    p.isFold &&
+    css`
+      transform: translate3d(100%, 0, 0);
+    `}
+`;
+
+const BandEmotions = styled.ol`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 `;
